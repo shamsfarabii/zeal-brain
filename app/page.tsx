3e339feb02c30e6a964bb72bef5755f6e31dc822
shams99 <shamsfarabialmuhit71@gmail.com>
2024-10-12 10:59:24 +0600

    fix: Refactoring Home page

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <Features />
       <Video />
       <Brands />
-      <AboutSectionOne />
+      {/* <AboutSectionOne /> */}
       <AboutSectionTwo />
       <Testimonials />
       <Pricing />
