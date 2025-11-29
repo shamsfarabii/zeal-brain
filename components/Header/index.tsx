a50334e4d200306fff061c4ed3f887c38076312e
SHAMS FARABI <farabi@SHAMSs-MacBook-Pro.local>
2025-11-30 01:30:31 +0600

    fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -48,7 +48,7 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
+            <div className="w-32 max-w-full px-4 xl:mr-12">
               <Link
                 href="/"
                 className={`header-logo block w-full ${sticky ? "py-5 lg:py-2" : "py-8"
